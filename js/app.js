eb9d79c40ad0f00a4206293a4e7e02fd6a2e20ad
Josh McBroom <mcbroomjc@gmail.com>
2016-07-21 20:39:34 -0700

    Almost done, need to work on coloring, filter, and win conditions.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
-/* setup your angular app here */
+var app = angular.module('FruitsAndVeggies', []);
+
+app.controller('MainCtrl', ['$scope', function($scope) {
+  $scope.foodArray = fruits.concat(vegetables);
+  $scope.fruitArray = [];
+  $scope.veggieArray = [];
+
+  $scope.leftClick = function(index) {
+    $scope.foodArray.push($scope.veggieArray[index]);
+    $scope.veggieArray.splice(index, 1);
+  };
+
+  $scope.leftClickMid = function(index) {
+    $scope.fruitArray.push($scope.foodArray[index]);
+    $scope.foodArray.splice(index, 1);
+  };
+
+  $scope.rightClickMid = function(index) {
+    $scope.veggieArray.push($scope.foodArray[index]);
+    $scope.foodArray.splice(index, 1);
+  };
+
+  $scope.rightClick = function(index) {
+    $scope.foodArray.push($scope.fruitArray[index]);
+    $scope.fruitArray.splice(index, 1);
+  }
+  // $scope.random = function(array) {
+  //   for (var i = 0; i < array.length; i++) {
+
+  //   }
+  // }
+}]);
 
 // debug stuff to show the app is loading and fruit / veggies are available
 console.log('App Started');
